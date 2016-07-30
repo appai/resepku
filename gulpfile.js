e478c47a8c2440f596708743554b3aec8f519fc2
Appai <akbarputrapai@gmail.com>
2016-07-30 12:55:49 +0700

    Update missing font and minimized html after deploy

--- gulpfile.js
+++ gulpfile.js
@@ -62,7 +62,7 @@ gulp.task('html', ['styles', 'scripts'], () => {
     .pipe($.useref({searchPath: ['.tmp', 'app', '.']}))
     .pipe($.if('*.js', $.uglify()))
     .pipe($.if('*.css', $.cssnano({safe: true, autoprefixer: false})))
-    .pipe($.if('*.html', $.htmlmin({collapseWhitespace: true})))
+    //.pipe($.if('*.html', $.htmlmin({collapseWhitespace: true})))
     .pipe(gulp.dest('dist'));
 });
 
